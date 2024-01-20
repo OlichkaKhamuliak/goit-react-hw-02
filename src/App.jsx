0c74817992f5769b46e2a7a3b9ad2c9b2d3a1bf5
OlichkaKhamuliak <olkoboyko97@gmail.com>
2024-01-20 13:08:12 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { Description } from './components/Description';
-import { Options } from './components/Options';
-import { Feedback } from './components/Feedback';
-import { Notification } from './components/Notification';
+import { Description } from './components/Description/Description';
+import { Options } from './components/Options/Options';
+import { Feedback } from './components/Feedback/Feedback';
+import { Notification } from './components/Notificate/Notification';
 
 export const App = () => {
   const [feedbackTypesCount, setFeedbackTypesCount] = useState(() => {
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,16 @@
+import css from './Options.module.css';
+
 const Button = ({ handleClick, children }) => {
-  return <button onClick={handleClick}>{children}</button>;
+  return (
+    <button onClick={handleClick} className={css.button}>
+      {children}
+    </button>
+  );
 };
 
 export const Options = ({ handleFeedbackTypeChange, handleReset, hasFeedback }) => {
   return (
-    <div>
+    <div className={css.container}>
       <Button handleClick={() => handleFeedbackTypeChange('good')}>Good</Button>
       <Button handleClick={() => handleFeedbackTypeChange('neutral')}>Neutral</Button>
       <Button handleClick={() => handleFeedbackTypeChange('bad')}>Bad</Button>
