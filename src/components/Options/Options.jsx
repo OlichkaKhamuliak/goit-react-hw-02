13eb0ca6a58b5fad4ac224733626a04369d8c0be
OlichkaKhamuliak <olkoboyko97@gmail.com>
2024-01-20 19:29:51 +0200

    add hover

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,9 +4,9 @@ import { RiEmotionNormalLine } from 'react-icons/ri';
 import { FaRegSadCry } from 'react-icons/fa';
 import { BiReset } from 'react-icons/bi';
 
-const Button = ({ handleClick, children }) => {
+const Button = ({ handleClick, children, type }) => {
   return (
-    <button onClick={handleClick} className={css.button}>
+    <button onClick={handleClick} className={`${css.button} ${type ? css[type] : ''}`}>
       {children}
     </button>
   );
@@ -15,17 +15,17 @@ const Button = ({ handleClick, children }) => {
 export const Options = ({ handleFeedbackTypeChange, handleReset, hasFeedback }) => {
   return (
     <div className={css.container}>
-      <Button handleClick={() => handleFeedbackTypeChange('good')}>
+      <Button handleClick={() => handleFeedbackTypeChange('good')} type="good">
         Good <BiHappyHeartEyes className={css.icon} />
       </Button>
-      <Button handleClick={() => handleFeedbackTypeChange('neutral')}>
+      <Button handleClick={() => handleFeedbackTypeChange('neutral')} type="neutral">
         Neutral <RiEmotionNormalLine className={css.icon} />
       </Button>
-      <Button handleClick={() => handleFeedbackTypeChange('bad')}>
+      <Button handleClick={() => handleFeedbackTypeChange('bad')} type="bad">
         Bad <FaRegSadCry className={css.icon} />
       </Button>
       {hasFeedback && (
-        <Button handleClick={handleReset}>
+        <Button handleClick={handleReset} type="reset">
           Reset <BiReset className={css.icon} />
         </Button>
       )}
